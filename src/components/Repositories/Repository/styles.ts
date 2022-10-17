838243b425c577ae582fe79a01f91bae97ce3385
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2022-10-17 12:37:46 -0200

    Repository component: change techs color

--- src/components/Repositories/Repository/styles.ts
+++ src/components/Repositories/Repository/styles.ts
@@ -3,9 +3,6 @@ import styled from 'styled-components';
 import colors from 'assets/colors.json';
 
 export const RepositoryContainer = styled.div`
-  /* max-width: 52rem;
-  width: calc(50% - 2rem); */
-
   .repository__name {
     margin-block: 2.4rem 1.6rem;
   }
@@ -16,10 +13,10 @@ export const RepositoryContainer = styled.div`
   }
 
   .repository__used-tecnologies {
+    max-width: 100%;
     font-weight: 500;
     color: ${colors.brand};
     margin-bottom: 3.2rem;
-    max-width: 100%;
 
     summary {
       margin-bottom: 1rem;
@@ -29,11 +26,11 @@ export const RepositoryContainer = styled.div`
 
     &__tech {
       display: inline-block;
-      background-image: ${colors.gradients.purple};
+      background: ${colors.brand};
       margin-bottom: 0.8rem;
       margin-right: 1.2rem;
       border-radius: 1.6rem;
-      padding-inline: 1.2rem;
+      padding: 0.4rem 1.2rem;
       color: ${colors.neutrals.light}
     }
   }
